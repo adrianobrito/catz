4c2730024a2fd9b67f3d48f5f6181639839dc33b
adrianobrito <adrianosdebrito@gmail.com>
2015-06-09 14:28:52 -0300

    Beggining Curses

--- view/terminal_view.rb
+++ view/terminal_view.rb
@@ -4,10 +4,12 @@ include Curses
 module View
 
 	class TerminalWindow
-		SCREEN_HEIGHT      = 24
+		SCREEN_HEIGHT      = 14
 		SCREEN_WIDTH       = 80
 		HEADER_HEIGHT      = 1
 		HEADER_WIDTH       = SCREEN_WIDTH
+		MAIN_WINDOW_HEIGHT = SCREEN_HEIGHT - HEADER_HEIGHT
+		MAIN_WINDOW_WIDTH  = SCREEN_WIDTH
 
 		def render
 			# init_screen
@@ -23,19 +25,10 @@ module View
 			# ensure
 			#   close_screen
 			# end	
-			noecho
-			nonl
-			stdscr.keypad(true)
-			raw
-			stdscr.nodelay = 1
-			start_color
-			init_pair(2, COLOR_BLACK, COLOR_WHITE)
-			init_pair(3, COLOR_BLACK, COLOR_WHITE)
-
-			init_screen
-			crmode
-			init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK)	
+			init		
 			show_header
+			show_editor
+			#getch
 		end
 
 		def show_message(message)
@@ -50,15 +43,57 @@ module View
 		  win.close
 		end
 
-		def show_header
-			header_window = Curses::Window.new(HEADER_HEIGHT, HEADER_WIDTH, 0, 0)
-			header_window.color_set(2)
-			header_window << "Catz".center(HEADER_WIDTH)
-			header_window.refresh
-		end
+		private 
+			def init
+				noecho
+				nonl
+				stdscr.keypad(true)
+				raw
+				stdscr.nodelay = 1
+				start_color
+				init_pair(2, COLOR_BLACK, COLOR_WHITE)
+				init_pair(3, COLOR_BLACK, COLOR_WHITE)
 
-		def show_editor
-		end
+				init_screen
+				crmode
+				init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK)
+			end
+
+			def show_header
+				header_window = Curses::Window.new(HEADER_HEIGHT, HEADER_WIDTH, 0, 0)
+				header_window.color_set(2)
+				header_window << "Catz".center(HEADER_WIDTH)
+				header_window.refresh
+			end
+
+			def show_editor
+				editor = Curses::Window.new(24, SCREEN_WIDTH, 1, 0)
+				editor.idlok(true)
+				editor.scrollok(true)
+				editor.setpos(0, 0)
+				editor.keypad(true)
+
+				init_editor(editor)
+
+				editor.refresh
+				editor
+			end
+
+			def init_editor(editor)
+				loop do
+					input = editor.getch
+					case input
+					when 13 #Enter
+						editor.addstr "\n"
+						editor.move(1,0)
+					when 8
+						editor.delch
+						editor.move(-1,0)
+					else
+						editor.addstr(input.to_s)	if input
+					end
+				end
+			end
 
 	end
 

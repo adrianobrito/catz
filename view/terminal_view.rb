44b8dc8be893e633d8872f70649d40116b08e3ae
adrianobrito <adrianosdebrito@gmail.com>
2015-06-09 13:09:38 -0300

    Beggining Curses

---
+++ view/terminal_view.rb
@@ -0,0 +1,65 @@
+require "curses"
+include Curses
+
+module View
+
+	class TerminalWindow
+		SCREEN_HEIGHT      = 24
+		SCREEN_WIDTH       = 80
+		HEADER_HEIGHT      = 1
+		HEADER_WIDTH       = SCREEN_WIDTH
+
+		def render
+			# init_screen
+			# begin
+			#   crmode
+			# #  show_message("Hit any key")
+			#   setpos((lines - 5) / 2, (cols - 10) / 2)
+			#   addstr("Hit any key")
+			#   refresh
+			#   getch
+			#   show_message("Hello, World!")
+			#   refresh
+			# ensure
+			#   close_screen
+			# end	
+			noecho
+			nonl
+			stdscr.keypad(true)
+			raw
+			stdscr.nodelay = 1
+			start_color
+			init_pair(2, COLOR_BLACK, COLOR_WHITE)
+			init_pair(3, COLOR_BLACK, COLOR_WHITE)
+
+			init_screen
+			crmode
+			init_pair(COLOR_BLUE,COLOR_BLUE,COLOR_BLACK)	
+			show_header
+		end
+
+		def show_message(message)
+		  width = message.length + 6
+		  win = Window.new(5, width,
+		            (lines - 5) / 2, (cols - width) / 2)
+		  win.box(?|, ?-)
+		  win.setpos(2, 3)
+		  win.addstr(message)
+		  win.refresh
+		  win.getch
+		  win.close
+		end
+
+		def show_header
+			header_window = Curses::Window.new(HEADER_HEIGHT, HEADER_WIDTH, 0, 0)
+			header_window.color_set(2)
+			header_window << "Catz".center(HEADER_WIDTH)
+			header_window.refresh
+		end
+
+		def show_editor
+		end
+
+	end
+
+end
